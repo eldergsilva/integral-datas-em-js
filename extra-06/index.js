51300154707545c49b306212527914217e4e6583
eldergsilva <eldergsilva@gmail.com>
2026-05-10 11:04:38 -0300

    ex 06

---
+++ extra-06/index.js
@@ -0,0 +1,23 @@
+const taValendo = (inicio, solicitacao) => {
+    const umDiaEmMs = 24 * 60 * 60 * 1000  
+    const fim = new Date(inicio.getTime() + umDiaEmMs)
+
+    const resultado = solicitacao >= inicio && solicitacao <= fim
+    console.log(resultado)
+    return resultado
+}
+
+// Testes
+taValendo(
+    new Date(2021, 3, 26, 8, 0),   
+    new Date(2021, 3, 26, 12, 0)  
+) 
+taValendo(
+    new Date(2021, 3, 26, 8, 0),   
+    new Date(2021, 3, 27, 8, 1)    
+)  
+
+taValendo(
+    new Date(2021, 3, 26, 8, 0),  
+    new Date(2021, 3, 27, 8, 0)    
+) 
